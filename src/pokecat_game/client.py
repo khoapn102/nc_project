1ae9573b9fc78d47b1a4cc1e5fd0580afca10fc7
lhmtriet <trietle95@gmail.com>
2016-12-19 10:16:40 +0700

    [POKECAT]

--- src/pokecat_game/client.py
+++ src/pokecat_game/client.py
@@ -2,35 +2,20 @@ import socket
 import time
 import threading
 
+# Using UDP
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-# class Handler(threading.Thread):
-# 	def __init__(self,socket):
-# 		threading.Thread.__init__(self)
-# 		self.socket = socket
-# 	def run(self):
-# 		while 1:
-# 			msg = self.socket.recvfrom(256)
-# 			if (msg[0].lower() == 'q'):
-# 				break
-# 			print "\n<===> ",msg[0]," <===>\n"
-		
-
-
-client_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-
-# t = Handler(client_socket)
-# t.start()
 data = "Connect-Khoa-26"
 flag = True
 
 while 1:
     # Send 'Connect Signal'
-	client_socket.sendto(data,("localhost",9000))
+	client_socket.sendto(data,("localhost", 9000))
 	
 	if(data.lower() == 'q'):
 		break
-	
-	data = raw_input("(Type q or Q to quit)")	
+
+	data = raw_input("(Type q or Q to quit)")
 	
 client_socket.close()
 # t.join()
--- src/pokecat_game/server.py
+++ src/pokecat_game/server.py
@@ -1,20 +1,29 @@
 # -*- coding: utf-8 -*-
 import socket
 import time
-import threading
+from threading import Thread, Timer
+
+from ord import player
 from ord.pokemon import *
 from ord.player import *
 import random
 import json
 
-# Binding Server
-server_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-server_socket.bind(("",9000))
+# The constant parameters of the PokeCat module
+pokeNum = 150
+worldSz = 1000
+maxPokeNum = 50
+spawning_time = 60
+despawning_time = 300
 
-# Initialize pokeworld - to generate Pokemon =
-poke_world = [[0 for x in range(1000)] for y in range(1000)]
+# Binding Server
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.bind(("", 9000))
+
+# Initialize pokeworld - to generate Pokemon
+poke_world = [[0 for x in range(worldSz)] for y in range(worldSz)]
 # Initialize playerworld - to move around
-player_world = [[0 for x in range(1000)] for y in range(1000)]
+player_world = [[0 for x in range(worldSz)] for y in range(worldSz)]
 # Get all Pokemons Data
 file = open('../../data/pokedex.json').read()
 data = json.loads(file)
@@ -22,62 +31,10 @@ data = json.loads(file)
 
 print "Game server is waiting for connection ..."
 
-# Handling multi players
-class Handler(threading.Thread):
-	def __init__(self,socket, data, adr):
-		threading.Thread.__init__(self)
-		self.data = data
-		self.socket = socket
-		self.adr = adr
-	def run(self):
-			# #print "run"
-			# if (self.data.lower() == "q" and address == self.adr):
-			# 	self.socket.sendto(self.data, self.adr) # send 'q' only to that client
-			# 	print str(self.adr) + " --> ended"
-			#
-			# if not (address == self.adr): # prevent from sending back msg to its sender
-			# 	reply = '[' + self.adr[0] + ':' + str(self.adr[1]) + '] - ' + data
-			# 	self.socket.sendto(reply, self.adr)
-			# 	print str(self.adr) + " --> sent"
-			#
-			return
-
-def automove():
-	return
-
-def catch_pokemon(player, index):
-	"""
-	From index, create a pokemon with random level
-	Add to Player's bag
-	:param player:
-	:param index:
-	:return:
-	"""
-	poke_a = pokemonDAO(data[index-1]).create_pokemon()
-	player.add_pokemon(poke_a)
-	return
-
 def generate_pos():
-	x = random.randint(0,999)
-	y = random.randint(0,999)
+	x = random.randint(0, worldSz - 1)
+	y = random.randint(0, worldSz - 1)
 	return (x,y)
-
-def generate_player(player):
-	"""
-	When player join server, randomize a location
-	If player spawn where pokemon, he will automatically catch it
-	:return:
-	"""
-	x,y = generate_pos()
-	while player_world[x][y] == 1:
-		x,y = generate_pos()
-	if player_world[x][y] == 0:
-		if poke_world[x][y] != 0:
-			index = poke_world[x][y]
-			catch_pokemon(player, index)
-			poke_world[x][y] = 0
-		player_world[x][y] = 1
-	return (x,y) # Return location for new Player
 
 def generate_pokemon():
 	"""
@@ -89,20 +46,24 @@ def generate_pokemon():
 	If no one catches it, pokemon will be despawned
 	:return:
 	"""
-	threading.Timer(60, generate_pokemon).start()
+
+	global player_world
+	global poke_world
+
+	Timer(spawning_time, generate_pokemon).start()
 	batch_loc = []
 	print 'Spawning Pokemon.............!!!'
 	count = 0
-	while count < 50:
+	while count < maxPokeNum:
 		# Pokemon random index 1 - 150
-		index = random.randint(1, 150)
+		index = random.randint(1, pokeNum)
 		x,y = generate_pos()
 		batch_loc.append((x,y))
 		if poke_world[x][y] == 0:
 			poke_world[x][y] = index
 			count += 1
 	print 'Complete Spawning !!!'
-	time.sleep(300) # Wait for 5 mins
+	time.sleep(despawning_time) # Wait for 5 mins
 	for loc in batch_loc:
 		x = loc[0]
 		y = loc[1]
@@ -112,31 +73,127 @@ def generate_pokemon():
 	print 'Despawned Pokemon............!!!'
 	return
 
-adr_list = {}
-# thread_list = []
+
+class PlayerHandler(Thread):
+
+	def __init__(self, name, age, address):
+		Thread.__init__(self)
+		self.name = name
+		self.age = age
+		x, y = self.generate_player()
+		self.x = x
+		self.y = y
+		self.adr = address
+		self.player = player(self.name, self.age, [self.x, self.y])
+
+	def run(self):
+
+		print "Start moving"
+		move_thread = self.automove()
+
+		time.sleep(120)
+
+		print "End moving"
+		# move_thread.cancel()
+
+		return
+
+	def automove(self):
+
+		global player_world
+
+		Timer(1, self.automove).start()
+
+		# Options: clockwise
+		# 1: Move up
+		# 2: Move right
+		# 3: Move down
+		# 4: Move left
+
+		oldX = self.x
+		oldY = self.y
+
+		option = random.randint(1, 4)
+
+		print "Player ", self.name, " ", option
+		if option == 1:
+			self.y += 1
+		elif option == 2:
+			self.x += 1
+		elif option == 3:
+			self.y -= 1
+		elif option == 4:
+			self.x -= 1
+
+		if self.x < 0:
+			self.x = worldSz - 1
+
+		if self.y < 0:
+			self.y = worldSz - 1
+
+		self.x %= worldSz
+		self.y %= worldSz
+
+		player_world[oldX][oldY] = 0
+		player_world[self.x][self.y] = 1
+
+	def generate_player(self):
+		"""
+		When player join server, randomize a location
+		If player spawn where pokemon, he will automatically catch it
+		:return:
+		"""
+		global player_world
+		global poke_world
+
+		x, y = generate_pos()
+		while player_world[x][y] == 1:
+			x, y = generate_pos()
+
+		if poke_world[x][y] != 0:
+			index = poke_world[x][y]
+			self.catch_pokemon(index)
+			poke_world[x][y] = 0
+		player_world[x][y] = 1
+
+		return (x, y)  # Return location for new Player
+
+	def catch_pokemon(self, index):
+		"""
+		From index, create a pokemon with random level
+		Add to Player's bag
+		:param player:
+		:param index:
+		:return:
+		"""
+
+		poke_a = pokemonDAO(data[index - 1]).create_pokemon()
+		self.player.add_pokemon(poke_a)
+		return
+
+
+adr_list = []
+playerThreads = []
+
+generatingThread = Thread(target=generate_pokemon).start()
 
 while 1:
-
 	# Player joined
 	data, address = server_socket.recvfrom(256)
 	# If new player
 	if address not in adr_list:
 		if 'Connect' in data:
 			temp = data.split('-')
-		x,y = generate_pos()
+
 		# Address will now have the new player
-		adr_list[address] = player(temp[0], temp[1], [x,y])
+		adr_list.append(address)
+
+		player_thread = PlayerHandler(temp[1], temp[2], address)
+		player_thread.start()
+		playerThreads.append(player_thread)
 
 	# print adr_list.keys()
 	print "(",address[0]," ", address[1],") said: ",data
-	# Spawning Pokemon
-	threading.Thread(target=generate_pokemon).start()
-	# for x in adr_list:
-	# 	t = Handler(server_socket,data,x)
-	# 	t.start()
-	# 	thread_list.append(t)
-	# if(data.lower() == 'q'):
-	# 	adr_list.remove(address)
-	# for th in thread_list:
-	# 	th.join()
+
+generatingThread.join()
 server_socket.close()
